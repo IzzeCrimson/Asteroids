bc25026f641c30db282279464d880411deb6e609
HarryWennberg <91454209+HarryWennberg@users.noreply.github.com>
2022-04-12 18:45:02 +0200

    Spawn

--- AsteroidsProject/Assets/Harry/Scripts/AsteroidSpawner.cs
+++ AsteroidsProject/Assets/Harry/Scripts/AsteroidSpawner.cs
@@ -6,7 +6,12 @@ public class AsteroidSpawner : MonoBehaviour
 {
     [SerializeField] BoxCollider boxCollider;
     [SerializeField] GameObject asteroid;
-    Transform asteroidSpawnPosition;
+
+    GameObject movingObject;
+    Vector3 SpawnPositoin;
+    float asteroidSpawnerCooldown = 5;
+
+    bool canSpawnAsteroid;
 
     [SerializeField] bool shootAsteroidsUp;
     [SerializeField] bool shootAsteroidsDown;
@@ -15,14 +20,71 @@ public class AsteroidSpawner : MonoBehaviour
 
     void Start()
     {
-        asteroidSpawnPosition = boxCollider.transform;
+ 
         Debug.Log(boxCollider.size);
-        Instantiate(asteroid,new Vector3(2,2,2), Quaternion.identity, boxCollider.transform);
+        canSpawnAsteroid = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (canSpawnAsteroid)
+        {
+
+            if (shootAsteroidsUp)
+            {
+
+                SpawnPositoin.x = Random.Range(-boxCollider.size.x / 2, boxCollider.size.x / 2);
+
+                movingObject = Instantiate(asteroid, SpawnPositoin, Quaternion.identity, boxCollider.transform);
+
+                movingObject.transform.Translate(new Vector3(2,0,0), Space.Self);
+
+                canSpawnAsteroid = false;
+
+            }
+            else if (shootAsteroidsDown)
+            {
+
+                SpawnPositoin.x = Random.Range(-boxCollider.size.x / 2, boxCollider.size.x / 2);
+
+                movingObject = Instantiate(asteroid, SpawnPositoin, Quaternion.identity, boxCollider.transform);
+                movingObject.transform.position = movingObject.transform.position * Time.deltaTime;
+
+                canSpawnAsteroid = false;
+
+            }
+            else if (shootAsteroidsLeft)
+            {
+
+                SpawnPositoin.x = Random.Range(-boxCollider.size.z / 2, boxCollider.size.z / 2);
+
+                movingObject = Instantiate(asteroid, SpawnPositoin, Quaternion.identity, boxCollider.transform);
+                movingObject.transform.position = movingObject.transform.position * Time.deltaTime;
+
+                canSpawnAsteroid = false;
+
+            }
+            else if (shootAsteroidsRight)
+            {
+
+                SpawnPositoin.x = Random.Range(-boxCollider.size.z / 2, boxCollider.size.z / 2);
+
+                movingObject = Instantiate(asteroid, SpawnPositoin, Quaternion.identity, boxCollider.transform);
+                movingObject.transform.position = Vector3.MoveTowards(movingObject.transform.position,new Vector3(100,2,100),10); 
+
+                canSpawnAsteroid = false;
+
+            }
+
+            StartCoroutine(Cooldown());
+        }
     }
+
+    IEnumerator Cooldown()
+    {
+        yield return new WaitForSeconds(asteroidSpawnerCooldown);
+
+        canSpawnAsteroid = true;
+    }     
 }
